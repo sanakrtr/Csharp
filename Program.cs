b078520e40989e6b175d9b7ee504df4983d305f4
sanakrtr <105944776+sanakrtr@users.noreply.github.com>
2023-02-27 18:38:19 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string name; 
+        int age; 
+
+        Console.WriteLine("What is your name?");
+        name = Console.ReadLine();
+        Console.WriteLine($"How old are you, {name}?");
+        age = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine($"Your name is {name}");
+        Console.WriteLine($"Your age is {age}");
+    }
+ }
