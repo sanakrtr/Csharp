69504786f6bca81085b3090074afccdb4f927b7d
sanakrtr <105944776+sanakrtr@users.noreply.github.com>
2023-02-27 18:35:20 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter a ");
+        int a = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Perimetr of {0} square : {1}", a, a * 4);
+        Console.WriteLine("Area of {0} square : {1}", a, a * a);
+
+        Console.ReadKey();
+    }
+}
