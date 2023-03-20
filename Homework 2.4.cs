f62c5a23492ce80941ab28bfb5d4195cf5566b72
sanakrtr <105944776+sanakrtr@users.noreply.github.com>
2023-03-20 11:25:40 +0000

    Add files via upload

---
+++ Homework 2.4.cs
@@ -0,0 +1,30 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Dog myDog = new Dog();
+        Console.WriteLine("Enter dog's name:");
+        myDog.name = Console.ReadLine();
+        Console.WriteLine("Enter dog's mark:");
+        myDog.mark = Console.ReadLine();
+        Console.WriteLine("Enter dog's age:");
+        myDog.age = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Dog information: " + myDog);
+
+
+    }
+
+    struct Dog
+    {
+        public string name;
+        public string mark;
+        public int age;
+
+        public override string ToString()
+        {
+            return $"Name: {name}, Mark: {mark}, Age: {age}";
+        }
+    }
+}
